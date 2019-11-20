f672e17912e631dfc2d703af8d713a7bf06f0316
juanjolibrero98 <alum.jjoselibreroc@iesalixar.org>
2019-11-20 15:08:00 +0100

    Tarea 8-10

---
+++ hola-mundo-en-java/HolaMundo.java
@@ -0,0 +1,12 @@
+public class HolaMundo {
+    public static void main(String[] args) {
+        
+        System.out.println("Introduzca la altura de los calcetines");
+        System.out.println("Introduzca la altura de los zapatos");
+        
+
+
+    }
+
+
+}
